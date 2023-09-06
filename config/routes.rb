3d3aeb870e42ca2812820009401abc3980bfc1e2
enoch <enokkang@gmail.com>
2023-09-06 14:15:16 -0500

    commented out unnecessary routes. minimized user access. changed favorite controller a bit that only users see their own favorites

--- config/routes.rb
+++ config/routes.rb
@@ -4,28 +4,23 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  get "/users" => "users#index"
+  #get "/users" => "users#index"
   post "/users" => "users#create"
-  get "/users/:id" => "users#show"
-  patch "/users/:id" => "users#update"
-  delete "/users/:id" => "users#destroy"
+  #get "/users/:id" => "users#show"
+  #patch "/users/:id" => "users#update"
+  #delete "/users/:id" => "users#destroy"
 
   get "/items", to: "items#index"
   get "/items/:id", to: "items#show"
-  post "/items", to: "items#create"
-  patch "/items/:id", to: "items#update"
-  delete "/items/:id", to: "items#destroy"
+  #post "/items", to: "items#create"
+  #patch "/items/:id", to: "items#update"
+  #delete "/items/:id", to: "items#destroy"
 
   get "/favorites", to: "favorites#index"
-  get "/favorites/:id", to: "favorites#show"
+  #get "/favorites/:id", to: "favorites#show"
   post "/favorites", to: "favorites#create"
-  patch "/favorites/:id", to: "favorites#update"
-  delete "/favorites/:id", to: "favorites#destroy"
+  #patch "/favorites/:id", to: "favorites#update"
+  #delete "/favorites/:id", to: "favorites#destroy"
 
-  get "/sessions", to: "sessions#create"
-
-  resource :users
-  resource :items
-  resource :favorites
-  resource :sessions
+  post "/sessions", to: "sessions#create"
 end
